170722097e482dc70b7b4d7f7db35e2624321828
mrogunlana <diran.ogunlana@gmail.com>
2020-03-26 23:46:36 -0400

    added Autofac IoC container and Nlog implementation

---
+++ Panama.Standard.IoC.Autofac/DefaultAutofacConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Features.AttributeFilters;
+using Panama.Standard.Commands;
+using System;
+using System.Linq;
+
+namespace Panama.Standard.IoC.Autofac
+{
+    public class DefaultAutofacConfiguration
+    {
+        public ContainerBuilder Register(ContainerBuilder builder)
+        {
+            //Register all validators -- singletons
+            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+                   .Where(t => t.IsAssignableTo<IValidation>())
+                   .Named<IValidation>(t => t.Name)
+                   .AsImplementedInterfaces()
+                   .SingleInstance();
+
+            //Register all commands -- singletons
+            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+                   .Where(t => t.IsAssignableTo<ICommand>())
+                   .Named<ICommand>(t => t.Name)
+                   .AsImplementedInterfaces()
+                   .SingleInstance()
+                   .WithAttributeFiltering();
+
+            return builder;
+        }
+    }
+}
